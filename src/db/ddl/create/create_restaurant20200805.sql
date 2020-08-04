ad37de4143e0d6bdfc1834c492fba96f1e882eef
takehir00 <0kutakehiro15@gmail.com>
2020-08-05 08:09:29 +0900

    店舗テーブル周り

---
+++ src/db/ddl/create/create_restaurant20200805.sql
@@ -0,0 +1,18 @@
+drop table restaurant;
+create table restaurant
+    (id int not null primary key AUTO_INCREMENT,
+     name varchar(100) not null,
+     address varchar(1000),
+     phone_number varchar(100),
+     image MEDIUMTEXT,
+     mime_type varchar(100),
+     introduction text,
+     genre_id int,
+     from_time varchar(10),
+     to_time varchar(10),
+     ward_id int not null,
+     created_at datetime,
+         updated_at datetime,
+         FOREIGN KEY (ward_id)
+             references ward(id)
+    );
