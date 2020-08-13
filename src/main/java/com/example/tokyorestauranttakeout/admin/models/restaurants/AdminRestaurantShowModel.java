3c7fc56b45e7170458b2e0450490ebbbf761dbf6
takehir00 <0kutakehiro15@gmail.com>
2020-08-14 08:09:08 +0900

    店舗詳細画面

--- src/main/java/com/example/tokyorestauranttakeout/admin/models/restaurants/AdminRestaurantShowModel.java
+++ src/main/java/com/example/tokyorestauranttakeout/admin/models/restaurants/AdminRestaurantShowModel.java
@@ -1,7 +1,9 @@
 package com.example.tokyorestauranttakeout.admin.models.restaurants;
 
-import java.util.Date;
+import lombok.Data;
 
+import java.util.Date;
+@Data
 public class AdminRestaurantShowModel {
     /** ID */
     public Integer id;
