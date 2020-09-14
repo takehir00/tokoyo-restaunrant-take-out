29493c86dbe82b7054e168d5d99abd8a2eada777
takehir00 <0kutakehiro15@gmail.com>
2020-09-15 08:41:56 +0900

    userDaoRealmma周り

--- src/main/java/com/example/tokyorestauranttakeout/security/UserDaoRealm.java
+++ src/main/java/com/example/tokyorestauranttakeout/security/UserDaoRealm.java
@@ -1,5 +1,8 @@
 package com.example.tokyorestauranttakeout.security;
 
+import com.example.tokyorestauranttakeout.entity.Account;
+import com.example.tokyorestauranttakeout.repositories.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -12,11 +15,17 @@ import java.util.List;
 
 @Component
 public class UserDaoRealm implements UserDetailsService {
+
+    @Autowired
+    AccountRepository accountRepository;
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         List<GrantedAuthority> authorityList = new ArrayList<>();
         authorityList.add(new SimpleGrantedAuthority("USER"));
 
-        return null;
+        Account account = accountRepository.selectByName(username);
+
+        return new LoginUser(account.getName(), account.getPassword(), authorityList);
     }
 }
