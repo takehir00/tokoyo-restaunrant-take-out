0fb4253cf9388b0a5adbb2743e5a6c3b2de7f3cf
takehir00 <0kutakehiro15@gmail.com>
2021-04-20 08:57:37 +0900

    クライアントアカウントリポジトリ

---
+++ src/main/java/com/example/tokyorestauranttakeout/repositories/ClientAccountRepository.java
@@ -0,0 +1,78 @@
+package com.example.tokyorestauranttakeout.repositories;
+
+import com.example.tokyorestauranttakeout.entity.ClientAccount;
+import com.example.tokyorestauranttakeout.entity.ClientAccountExample;
+import com.example.tokyorestauranttakeout.mapper.ClientAccountMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ClientAccountRepository {
+
+    @Autowired
+    private final ClientAccountMapper mapper;
+
+    public ClientAccountRepository(ClientAccountMapper mapper) {
+        this.mapper = mapper;
+    }
+
+    /**
+     * メールアドレスで検索
+     * @param email
+     * @return
+     */
+    public ClientAccount selectByEmail(String email) {
+        ClientAccountExample example = new ClientAccountExample();
+        ClientAccountExample.Criteria criteria = example.createCriteria();
+        criteria.andEmailEqualTo(email);
+
+        List<ClientAccount> clientAccountList = mapper.selectByExample(example);
+        //メールアドレスは一意なので１件目を取得
+        return clientAccountList.get(0);
+    }
+
+    /**
+     * IDで取得
+     * @param accountId ID
+     * @return クライアントアカウント
+     */
+    public ClientAccount selectById(Integer accountId) {
+        return mapper.selectByPrimaryKey(accountId);
+    }
+
+    /**
+     * 全権取得
+     * @return クライアントアカウントリスト
+     */
+    public List<ClientAccount> selectAll() {
+        ClientAccountExample example = new ClientAccountExample();
+        return mapper.selectByExample(example);
+    }
+
+    /**
+     * 登録
+     * @param clientAccount クライアントアカウント
+     */
+    public void create(ClientAccount clientAccount) {
+        mapper.insert(clientAccount);
+    }
+
+    /**
+     * 更新
+     * @param clientAccount
+     */
+    public void update(ClientAccount clientAccount) {
+        mapper.updateByPrimaryKey(clientAccount);
+    }
+
+    /**
+     * 削除
+     * @param id
+     */
+    public void delete(Integer id) {
+        mapper.deleteByPrimaryKey(id);
+    }
+
+}
