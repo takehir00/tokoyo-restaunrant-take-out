52425e7e9f443a401757c73ee90ddfa7cca143a5
takehir00 <0kutakehiro15@gmail.com>
2021-04-20 08:57:16 +0900

    メールアドレスフィールドを追加

--- src/db/ddl/create/create_client_account_20210416.sql
+++ src/db/ddl/create/create_client_account_20210416.sql
@@ -1,7 +1,9 @@
 drop table if exists client_account;
 create table client_account
     (id int not null primary key AUTO_INCREMENT,
-    name varchar(100) unique not null,
-    password varchar(1000) unique not null,
+    email varchar(100) unique not null,
+    password varchar(1000),
+    name varchar(100),
+    image_url varchar(1000),
     created_at datetime,
     updated_at datetime);
