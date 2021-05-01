18e580509532666560175fc2f6cbe414b42823bc
takehir00 <0kutakehiro15@gmail.com>
2021-05-01 18:39:58 +0900

    認証不要パスセキュリティ設定

---
+++ src/main/java/com/example/tokyorestauranttakeout/security/PublicSecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.tokyorestauranttakeout.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * 認証不要パスセキュリティ設定
+ */
+@Configuration
+@EnableWebSecurity
+public class PublicSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    /**
+     * @param httpSecurity
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        String[] permittedUrls = {"/css/**","/images/**", "/webjars/bootstrap/4.3.1/css/bootstrap.css", "/pub/**"};
+
+        //認証がかからないエンドポイントを明示する。
+        httpSecurity.authorizeRequests()
+                .antMatchers(permittedUrls)
+                .permitAll();
+
+    }
+}
