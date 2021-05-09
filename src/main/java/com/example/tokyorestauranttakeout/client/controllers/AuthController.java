d05140a16a25e233ddb1c111c9adc01551c02367
takehir00 <0kutakehiro15@gmail.com>
2021-05-09 16:03:55 +0900

    不要インポート除去

--- src/main/java/com/example/tokyorestauranttakeout/client/controllers/AuthController.java
+++ src/main/java/com/example/tokyorestauranttakeout/client/controllers/AuthController.java
@@ -5,16 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
-import org.springframework.web.servlet.FlashMap;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.support.RequestContextUtils;
-import org.springframework.web.servlet.support.SessionFlashMapManager;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.util.Map;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(ClientServerPaths.AUTH)
