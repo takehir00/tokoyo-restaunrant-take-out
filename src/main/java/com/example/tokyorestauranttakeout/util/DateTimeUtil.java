fd372134de4bec67ae5b9242da8b35796c033837
takehir00 <0kutakehiro15@gmail.com>
2021-07-02 15:52:11 +0900

    DateTimeUtility

--- src/main/java/com/example/tokyorestauranttakeout/util/DateTimeUtil.java
+++ src/main/java/com/example/tokyorestauranttakeout/util/DateTimeUtil.java
@@ -1,7 +1,10 @@
 package com.example.tokyorestauranttakeout.util;
 
+import java.sql.Time;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
+import java.sql.Date;
+import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
 
@@ -10,7 +13,32 @@ public class DateTimeUtil {
      */
     private DateTimeUtil() {}
 
+    /**
+     * 現在日時を生成
+     * @return 現在日時
+     */
     public static LocalDateTime now() {
-        return LocalDateTime.now();
+        return LocalDateTime.now().withNano(0);
+    }
+
+    /**
+     * DateインスタンスとTimeインスタンスからLocalDateTimeインスタンスを生成する
+     * @param date Dateインスタンス
+     * @param time Timeインスタンス
+     * @return LocalDateTimeインスタンス
+     */
+    public static LocalDateTime toLocalDatetime(Date date, Time time) {
+        return LocalDateTime.of(date.toLocalDate(),time.toLocalTime());
+    }
+
+    /**
+     * 日時を成形する
+     * @param localDateTime LocalDateTimeインスタンス
+     * @return 成形した日時
+     */
+    public static String format(LocalDateTime localDateTime) {
+        DateTimeFormatter formatter =
+                DateTimeFormatter.ofPattern("yyyy MM dd HH. mm. ss");
+        return localDateTime.format(formatter);
     }
 }
